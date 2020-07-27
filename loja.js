534da7eae4faae07ac2f31a98cca0e60789bf3ec
Vinicius Depizzol <vdepizzol@gmail.com>
2020-07-26 21:07:53 -0700

    Create loja.js

---
+++ loja.js
@@ -0,0 +1,10 @@
+var url = window.location.pathname;
+var page = url.split('/');
+
+document.querySelector('body').dataset.url = page[2]
+
+
+// if (document.querySelector('body').dataset.url == 'produtos-detalhes') {
+// 	
+// 
+// }
