f7a82a77e5e2500f6c44170c304ce0e87cca0575
Vinicius Depizzol <vdepizzol@gmail.com>
2020-08-23 15:00:19 -0700

    Update loja.js

--- loja.js
+++ loja.js
@@ -4,7 +4,9 @@ var page = url.split('/');
 document.querySelector('body').dataset.url = page[2]
 
 
-// if (document.querySelector('body').dataset.url == 'produtos-detalhes') {
-// 	
-// 
-// }
+if (document.querySelector('body').dataset.url == 'produtos-detalhes') {
+	
+
+} else if (document.querySelector('body').dataset.url == 'home') {
+	window.location.href = 'https://loja-bd5244.webflow.io/';
+}
